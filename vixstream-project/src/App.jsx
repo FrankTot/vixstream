43fffbb9e6ba9f9968c574b78013ecadc61bba4f
FrankTot <francesco.totaro@edu-its.it>
2025-06-19 09:42:45 +0200

    Add files via upload

---
+++ vixstream-project/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Benvenuto su Vixstream!</h1>
+    </div>
+  )
+}
